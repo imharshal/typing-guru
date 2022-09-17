383dbf427e7160e0238fb5a75f0e63f7ac296a9c
Harshal <harshalbokade984@gmail.com>
2022-09-17 18:00:16 +0530

    +dynamic lesson generator , fixed: key hint issue

--- src/components/layouts/Playground.jsx
+++ src/components/layouts/Playground.jsx
@@ -1,28 +1,31 @@
 import { useState, useEffect } from "react";
 import ParagraphPractice from "./ParagraphPractice";
 import SentencePractice from "./SentencePractice";
+import { paragraph } from "txtgen";
+import { createLesson } from "../../services/LessonsGenerator";
 const Playground = ({ layout }) => {
-  const [keyPressed, setEvent] = useState();
+  const [keyPressed, setKeyPressed] = useState({ key: "" });
   const [lesson, setLesson] = useState([]);
   const [para, setPara] = useState([]);
 
   useEffect(() => {
+    // console.log(paragraph());
     const text =
-      "This is a Rju Ox Poe Tor text This is a Rju Ox Poe Tor text This is a Rju Ox Poe Tor text This is a Rju Ox Poe Tor text";
-    setLesson(text.split(""));
-    setPara(text.split(" "));
+      "This is a Rjjjjjj u Ox Poe Tor text This is a Rju Ox Poe Tor text This is a Rju Ox Poe Tor text This is a Rju Ox Poe Tor text";
+
+    setLesson(createLesson(['as df', 'jk l;', "asdf jkl;"], 5).split(''));
+    setPara(paragraph().split(" "));
   }, []);
 
   useEffect(() => {
     const onPress = (e) => {
-      setEvent(e);
+      setKeyPressed(e);
     };
-
     window.addEventListener("keydown", onPress);
 
     if (keyPressed && keyPressed.key != "") {
       setTimeout(function () {
-        setEvent({ key: "" });
+        setKeyPressed({ key: "" });
       }, 300);
     }
     // handleHintKey();
